56781e115134cbfa7637cf766ab68fd95b2cdafe
bruno wandrey <mabawandrey@gmail.com>
2025-03-02 23:36:21 -0300

    Added first ms

---
+++ df-mock-img-metadata-service/src/main/java/com/digital_foundries/mock_image_metadata_service/MockImageMetadataServiceApplication.java
@@ -0,0 +1,13 @@
+package com.digital_foundries.mock_image_metadata_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockImageMetadataServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockImageMetadataServiceApplication.class, args);
+	}
+
+}
