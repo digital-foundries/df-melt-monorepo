268e979aac5ef78dba781fb3a111be255855662c
bruno wandrey <mabawandrey@gmail.com>
2025-03-14 03:00:29 -0300

    SDS project created

---
+++ service-discovery-service/src/main/java/com/digital_foundries/service_discovery_service/ServiceDiscoveryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.digital_foundries.service_discovery_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceDiscoveryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceDiscoveryServiceApplication.class, args);
+	}
+
+}
---
+++ service-discovery-service/src/test/java/com/digital_foundries/service_discovery_service/ServiceDiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digital_foundries.service_discovery_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
